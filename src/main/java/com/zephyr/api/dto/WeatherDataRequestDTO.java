1031f0fdfa9702f1d95571877dc5ac5dfe0155ef
Pamella Binotto <pamellabinotto@gmail.com>
2026-05-11 20:48:53 -0300

    feat: implement DTO validation with jakarta validation

--- src/main/java/com/zephyr/api/dto/WeatherDataRequestDTO.java
+++ src/main/java/com/zephyr/api/dto/WeatherDataRequestDTO.java
@@ -1,27 +1,39 @@
 package com.zephyr.api.dto;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Max;
+
 
 public class WeatherDataRequestDTO {
 
-    private double temperature;
-    private double humidity;
+    @NotNull
+    @Min(-100)
+    @Max(100)
+    private Double temperature;
+
+    @NotNull
+    @Min(0)
+    @Max(100)
+    private Double humidity;
 
     public WeatherDataRequestDTO() {
 
     }
 
-    public double getTemperature() {
+
+    public Double getTemperature() {
         return temperature;
     }
 
-    public void setTemperature(double temperature) {
+    public void setTemperature(Double temperature) {
         this.temperature = temperature;
     }
 
-    public double getHumidity() {
+    public Double getHumidity() {
         return humidity;
     }
 
-    public void setHumidity(double humidity) {
+    public void setHumidity(Double humidity) {
         this.humidity = humidity;
     }
 }
