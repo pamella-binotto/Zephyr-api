7f50aefb272e1e2e9aadcc147408abb8bc5ceb9b
Pamella Binotto <pamellabinotto@gmail.com>
2026-06-03 22:50:30 -0300

    feat: add cache support for current, daily and hourly forecasts

--- src/main/java/com/zephyr/api/ZephyrApiApplication.java
+++ src/main/java/com/zephyr/api/ZephyrApiApplication.java
@@ -3,7 +3,7 @@ package com.zephyr.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableScheduling;
+;
 
 @SpringBootApplication
 @EnableCaching
