ed721402fd0fc0f8aeeda8842abe0cd4ae104a5c
Pamella Binotto <pamellabinotto@gmail.com>
2026-06-11 15:59:54 -0300

    feat(auth): add login flow with user and password validation

--- src/main/java/com/zephyr/api/service/AuthService.java
+++ src/main/java/com/zephyr/api/service/AuthService.java
@@ -1,9 +1,12 @@
 package com.zephyr.api.service;
 
+import com.zephyr.api.dto.LoginRequestDTO;
 import com.zephyr.api.dto.RegisterRequestDTO;
 import com.zephyr.api.dto.response.UserResponseDTO;
 import com.zephyr.api.entity.User;
+import com.zephyr.api.exception.InvalidPasswordException;
 import com.zephyr.api.exception.UserAlreadyExistsException;
+import com.zephyr.api.exception.UserNotFoundException;
 import com.zephyr.api.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -31,6 +34,18 @@ public class AuthService {
         );
 
         return repository.save(user);
+    }
+
+    public User login (LoginRequestDTO dto) {
+
+        Optional<User> existingUser = repository.findByEmail(dto.getEmail());
+        if (existingUser.isEmpty()) {
+            throw new UserNotFoundException("User not found");
+        }
+        if(!dto.getPassword().equals(existingUser.get().getPassword())) {
+            throw new InvalidPasswordException("Incorrect password");
+        }
+        return existingUser.get();
     }
 
 }
