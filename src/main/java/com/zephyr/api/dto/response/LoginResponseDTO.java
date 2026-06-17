79ade2059d0d302fbf2b7581f6d0718da9dea229
Pamella Binotto <pamellabinotto@gmail.com>
2026-06-16 22:53:29 -0300

    adjusted constructor login

--- src/main/java/com/zephyr/api/dto/response/LoginResponseDTO.java
+++ src/main/java/com/zephyr/api/dto/response/LoginResponseDTO.java
@@ -4,6 +4,10 @@ public class LoginResponseDTO {
 
     private String token;
 
+
+    public LoginResponseDTO() {
+    }
+
     public LoginResponseDTO(String token) {
         this.token = token;
     }
