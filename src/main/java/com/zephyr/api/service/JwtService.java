43142698b7aa01816e664876f1970e2b94488b62
Pamella Binotto <pamellabinotto@gmail.com>
2026-06-17 09:58:46 -0300

    feat(auth): implement JWT validation and email extraction

--- src/main/java/com/zephyr/api/service/JwtService.java
+++ src/main/java/com/zephyr/api/service/JwtService.java
@@ -16,17 +16,38 @@ public class JwtService {
     @Value("${jwt.secret}")
     private String jwtSecret;
 
-    private SecretKey getSigningKey(){
+    private SecretKey getSigningKey() {
         return Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
     }
 
-    public String generateToken (String email){
+    public String generateToken(String email) {
         return Jwts.builder()
                 .subject(email)
                 .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis()+86400000))
+                .expiration(new Date(System.currentTimeMillis() + 86400000))
                 .signWith(getSigningKey())
                 .compact();
     }
 
+    public String extractEmail(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(getSigningKey())
+                    .build()
+                    .parseSignedClaims(token);
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
