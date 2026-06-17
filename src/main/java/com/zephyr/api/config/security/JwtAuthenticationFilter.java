afa1657505acd287fb663a88776afaf8f26e33c0
Pamella Binotto <pamellabinotto@gmail.com>
2026-06-17 10:28:40 -0300

    feat(auth): create JWT service and authentication filter structure

---
+++ src/main/java/com/zephyr/api/config/security/JwtAuthenticationFilter.java
@@ -0,0 +1,42 @@
+package com.zephyr.api.config.security;
+
+import com.zephyr.api.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+
+    public JwtAuthenticationFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authHeader.substring(7);
+
+        filterChain.doFilter(request, response);
+    }
+}
+
